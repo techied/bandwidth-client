21a52312f529e6288ddcfd7c0fc256fc11a99b47
techied <11001514+techied@users.noreply.github.com>
2021-12-10 13:23:34 -0500

    update installscript

--- install-client.sh
+++ install-client.sh
@@ -1,1 +1,51 @@
 #!/usr/bin/env bash
+
+if [[ -v DOWNLOADED ]]; then
+  if [[ $DOWNLOADED == 0 ]]; then
+    echo "Download complete"
+    apt install -y dialog iperf3 chromium-chromedriver
+    echo "Enter server address"
+    CONNECTION_STRING=$(dialog --clear --title "Enter server address" --inputbox "Enter the connection address (IP or FQDN for your bandwidth-server)" 15 40 2>&1 >/dev/tty)
+    echo "Connection string: $CONNECTION_STRING"
+
+    service="
+    [Unit]
+    Description=\"Bandwidth Client\"
+
+    [Service]
+    ExecStart=/usr/bin/python3 /root/bandwidth-client/main.py
+    WorkingDirectory=/root/bandwidth-client
+    Restart=always
+    RestartSec=10
+    StandardOutput=syslog
+    StandardError=syslog
+    SyslogIdentifier=BandwidthClient
+
+    [Install]
+    WantedBy=multi-user.target
+    "
+
+    echo "$service" | sudo tee /etc/systemd/system/bandwidth-client.service
+
+    sudo systemctl daemon-reload
+    sudo systemctl enable bandwidth-client
+    sudo systemctl start bandwidth-client
+
+    exit 0
+  fi
+fi
+
+mkdir bandwidth-client
+
+cd bandwidth-client || exit
+
+wget https://dl.techied.me/latest-client.tar.gz
+
+tar -xvzf latest-client.tar.gz
+
+rm latest-client.tar.gz
+
+chmod +x install-client.sh
+
+export DOWNLOADED=0
+./install-client.sh
