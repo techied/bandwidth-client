9e02ebb1f27a52125833749447da60ad2a37311b
techied <11001514+techied@users.noreply.github.com>
2021-12-10 15:22:54 -0500

    fix root chrome

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def run():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
 
     @sio.on('connect')
